e7d1636542b04f34630ca70445109b3c72fa66dc
wimvc <wim.vdc@hotmail.com>
2021-04-14 07:03:25 +0200

    round number validation

--- routes/insight.js
+++ routes/insight.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/insight');
 const { cache } = require("../utils/middelware");
+const { getCurrentRound } = require("../utils/config");
 
 router.get('/points/all', cache(90), async (req, res, next) => {
   const result = await getPointsPerRound(req, "all")
@@ -13,15 +14,16 @@ router.get('/points/me', async (req, res, next) => {
   res.json(result)
 });
 
-async function  getPointsPerRound(req, who){
-  const round = Number(req.query.round);
+async function getPointsPerRound(req, who) {
+  let round = Number(req.query.round);
+  round = round >= getCurrentRound() ? round - 1 : round;
   if (round && who === 'me') {
     return await db.pointsPerRoundPerUser(round, req.user.uuid);
-} else if(round && who === 'all') {
-  return await db.pointsPerRound(round);
-} else {
-  return [];
-}
+  } else if (round && who === 'all') {
+    return await db.pointsPerRound(round);
+  } else {
+    return [];
+  }
 
 }
 
